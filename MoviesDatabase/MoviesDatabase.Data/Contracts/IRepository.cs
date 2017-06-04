7dc550dfb971ecc7dd895595c0c3fa981401f28d
Yoana Georgieva <yo.ana.georgieva@abv.bg>
2017-06-04 11:32:11 +0300

    Add Repository logic

---
+++ MoviesDatabase/MoviesDatabase.Data/Contracts/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesDatabase.Data.Contracts
+{
+    public interface IRepository<T>
+        where T : class
+    {
+        T GetById(object id);
+
+        IQueryable<T> Entities { get; }
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
---
+++ MoviesDatabase/MoviesDatabase.Data/Repository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using MoviesDatabase.Data.Contracts;
+using System.Data.Entity;
+using System.Linq;
+
+namespace MoviesDatabase.Data
+{
+    public class Repository<T> : IRepository<T>
+        where T : class
+    {
+        public Repository(DbContext dbContext)
+        {
+            if (dbContext == null)
+            {
+                throw new ArgumentNullException("Context cannot be null");
+            }
+
+            this.Context = dbContext;
+            this.Set = this.Context.Set<T>();
+        }
+
+        protected IDbSet<T> Set { get; set; }
+
+        protected DbContext Context { get; set; }
+
+        public T GetById(object id)
+        {
+            return this.Set.Find(id);
+        }
+
+        public IQueryable<T> Entities
+        {
+            get { return this.Set; }
+        }
+
+        public void Add(T entity)
+        {
+            var entry = this.Context.Entry(entity);
+            entry.State = EntityState.Added;
+            this.Context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            var entry = this.Context.Entry(entity);
+            entry.State = EntityState.Modified;
+            this.Context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            var entry = this.Context.Entry(entity);
+            entry.State = EntityState.Deleted;
+            this.Context.SaveChanges();
+        }
+    }
+}
