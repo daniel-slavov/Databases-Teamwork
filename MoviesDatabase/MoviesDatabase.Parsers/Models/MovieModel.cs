3d345dd9c5d9c97b62579d9eae802c07adeea140
Yoana Georgieva <yo.ana.georgieva@abv.bg>
2017-06-04 11:33:27 +0300

    Add MovieModel and ProducerService

---
+++ MoviesDatabase/MoviesDatabase.Parsers/Models/MovieModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesDatabase.Parsers.Models
+{
+    public class MovieModel
+    {
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public string Description { get; set; }
+
+        public int Length { get; set; }
+
+        public string ProducerName { get; set; }
+
+        public string StudioName { get; set; }
+
+        public string BookTitle { get; set; }
+
+        public ICollection<string> Stars { get; set; }
+
+        public ICollection<string> Genres { get; set; }
+    }
+}
---
+++ MoviesDatabase/MoviesDatabase.Services/ProducerService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using MoviesDatabase.Data.Contracts;
+using MoviesDatabase.Factories;
+using MoviesDatabase.Models;
+using MoviesDatabase.Parsers.Contracts;
+
+namespace MoviesDatabase.Services
+{
+    public class ProducerService
+    {
+        private readonly IRepository<Producer> producerRepository;
+        private readonly IJSONParser jsonParser;
+        private readonly IProducerFactory producerFactory;
+
+        public ProducerService(IRepository<Producer> producerRepository, IJSONParser jsonParser, IProducerFactory producerFactory)
+        {
+            if (producerRepository == null)
+            {
+                throw new ArgumentNullException("Producer repository cannot be null!");
+            }
+
+            if (jsonParser == null)
+            {
+                throw new ArgumentNullException("Json parser cannot be null!");
+            }
+
+            if (producerFactory == null)
+            {
+                throw new ArgumentNullException("Producer factory cannot be null!");
+            }
+
+            this.producerRepository = producerRepository;
+            this.jsonParser = jsonParser;
+            this.producerFactory = producerFactory;
+        }
+
+        public void AddProducers(IList<Producer> producers)
+        {
+            foreach (var producer in producers)
+            {
+                this.producerRepository.Add(producer);
+            }
+        }
+
+        public Producer CreateProducer(string name)
+        {
+            var producer = this.producerFactory.CreateProducer(name);
+            this.producerRepository.Add(producer);
+
+            return producer;
+        }
+    }
+}
